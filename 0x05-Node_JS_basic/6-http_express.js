29a34f2e67bd0ee351b1383799635ab792896760
odarym <mosesodary101@gmail.com>
2024-09-18 01:20:33 +0300

    Task 6 bug fix

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-
-app.listen(1245, () => {});
+app.listen(port);
 
 module.exports = app;
